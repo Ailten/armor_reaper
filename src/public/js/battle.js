2b8b2ab188a72bcd884e4fbc4b83cfe6bd6beda3
Ailten <faouzi26b@live.fr>
2026-09-19 13:32:39 +0200

    anime battle front end

--- src/public/js/battle.js
+++ src/public/js/battle.js
@@ -7,7 +7,7 @@ window.addEventListener('load', async () => {
 
     for(let i=0; i<battleLog.length; i++){
         await battleLogAnime(battleLog[i][0], battleLog[i][1]);
-        await sleep(1.6);
+        await sleep(0.3);
     }
 
     // TODO. end of fight print (loose screen, or loot and gains).
@@ -60,11 +60,28 @@ function isDieLog(log){
 
 
 async function playAnimeLog(battleLog, mainCharacterId) {
+    //if(mainCharacterId == -1)
+    //    return;
+
+    let mainCharDom = document.querySelector(`div[character-id="${mainCharacterId}"].battle-character-dom`);
 
     // use a spell.
     if(isUseSpellLog(battleLog)){
 
-        // TODO: make mainCharacter move.
+        if(mainCharDom.hasAttribute('character-is-left')){
+            mainCharDom.classList.add('character-move-left');
+        }else{
+            mainCharDom.classList.add('character-move-right');
+        }
+
+        await sleep(1);
+
+        if(mainCharDom.classList.contains('character-move-left')){
+            mainCharDom.classList.remove('character-move-left');
+        }
+        if(mainCharDom.classList.contains('character-move-right')){
+            mainCharDom.classList.remove('character-move-right');
+        }
 
         return;
     }
@@ -72,7 +89,31 @@ async function playAnimeLog(battleLog, mainCharacterId) {
     // affect stats (hp,mp,sp).
     if(isAffectStatsLog(battleLog)){
 
-        // TODO: make mainCharacter lose stats (over head).
+        let battleScreenDom = document.getElementById('battle-sreen');
+
+        // get str.
+        let strStatsEdit = battleLog.match(/[\+\-][0-9]+ [HMS]P$/)[0];
+
+        // build a dom stats message.
+        let statsTxtDom = battleScreenDom.appendChild(document.createElement('div'));
+        statsTxtDom.classList.add('stats-edit-fx');
+        statsTxtDom.classList.add(
+            strStatsEdit.endsWith('HP') ? 'stats-edit-hp':
+            strStatsEdit.endsWith('MP') ? 'stats-edit-mp':
+            'stats-edit-sp');
+        statsTxtDom.style.zIndex = '200';
+        statsTxtDom.innerText = strStatsEdit;
+
+        // set pos.
+        let posX = mainCharDom.offsetLeft + (mainCharDom.offsetWidth - statsTxtDom.offsetWidth) * 0.5;
+        statsTxtDom.style.left = `${posX}px`;
+        let posY = mainCharDom.offsetTop - statsTxtDom.offsetHeight;
+        statsTxtDom.style.top = `${posY}px`;
+        
+        await sleep(1);
+
+        // delete the fx message.
+        battleScreenDom.removeChild(statsTxtDom);
 
         return;
     }
@@ -80,7 +121,11 @@ async function playAnimeLog(battleLog, mainCharacterId) {
     // die.
     if(isDieLog(battleLog)){
 
-        // TODO: make mainCharacter fade out.
+        mainCharDom.classList.add('character-die');
+
+        await sleep(1);
+
+        mainCharDom.parentNode.removeChild(mainCharDom);
 
         return;
     }
@@ -107,7 +152,10 @@ function createCharacterDom(team, isLeft=true){
 
         let div = battleScreen.appendChild(document.createElement('div'));
         div.classList.add('battle-character-dom');
-        div.setAttribute('character-id', characterData.id)
+        div.setAttribute('character-id', characterData.id);
+        if(isLeft){
+            div.setAttribute('character-is-left', 'true');
+        }
         div.style.backgroundImage = `url("${baseUrl}public/img/characters/${characterData.skin}.png")`;
 
         // symetry horizontaly (for right team).
